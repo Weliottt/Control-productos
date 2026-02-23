e67b461967f5eb30107e84f5463f884d07095f59
Elio Mari <eliomari48@gmail.com>
2026-02-23 18:13:06 -0300

    fix: cuando un admin se edita a sí mismo se le pide que inice sesion para guardar cambios

--- src/main/java/lpj/controlproductos/controllers/UsuarioController.java
+++ src/main/java/lpj/controlproductos/controllers/UsuarioController.java
@@ -35,26 +35,27 @@ public class UsuarioController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/admin/usuarios")
-    public String listaUsuarios(Model model){
+    public String listaUsuarios(Model model) {
         List<Usuario> usuarios = usuarioService.getUsuarios();
-        model.addAttribute("usuarios",usuarios);
+        model.addAttribute("usuarios", usuarios);
         return "usuario/listaUsuarios";
     }
 
     @PostMapping("/admin/usuario/eliminar/{idUsuario}")
-    public String eliminarUsuario(@PathVariable Long idUsuario, RedirectAttributes redirectAttributes){
+    public String eliminarUsuario(@PathVariable Long idUsuario, RedirectAttributes redirectAttributes) {
         Usuario usuarioEliminado = usuarioService.getUsuarioById(idUsuario);
 
         //Verificamos que el usuario a eliminar no sea el ultimo admin disponible
-        if (usuarioService.esUltimoAdmin(usuarioEliminado)){
-            redirectAttributes.addFlashAttribute("noHayAdmin",true);
+        if (usuarioService.esUltimoAdmin(usuarioEliminado)) {
+            redirectAttributes.addFlashAttribute("noHayAdmin", true);
             return "redirect:/admin/usuarios";
         }
 
         //Verificamos que el usuario a eliminar no sea el mismo que esta con la sesion activa
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(usuarioEliminado.getUsername().equals(auth.getName())){
-            redirectAttributes.addFlashAttribute("mismoUsuario",true);
+        if (usuarioEliminado.getUsername().equals(auth.getName())) {
+            log.info("se ha editado el mismo usuario de la sesion");
+            redirectAttributes.addFlashAttribute("mismoUsuario", true);
             return "redirect:/admin/usuarios";
         }
 
@@ -65,41 +66,57 @@ public class UsuarioController {
     }
 
     @GetMapping("/admin/usuario/editar/{idUsuario}")
-    public String editarUsuario(@PathVariable Long idUsuario, Model model){
+    public String editarUsuario(@PathVariable Long idUsuario, Model model) {
 
 
         Usuario usuario = usuarioService.getUsuarioById(idUsuario);
-        List<Rol> roles =rolService.getRoles();
-        model.addAttribute("usuario",usuario);
-        model.addAttribute("roles",roles);
+        List<Rol> roles = rolService.getRoles();
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("roles", roles);
         return "usuario/editarUsuario";
     }
 
     @PostMapping("/admin/usuario/guardar")
-    public String guardarUsuario(Usuario usuarioform,RedirectAttributes redirectAttributes){
+    public String guardarUsuario(Usuario usuarioform, RedirectAttributes redirectAttributes, Authentication authentication) {
 
-
+        //Obtenemos el usuario guardado en la BD
         Usuario usuarioDB = usuarioService.getUsuarioById(usuarioform.getIdUsuario());
 
-        if(usuarioService.esUltimoAdmin(usuarioDB) &&
-                usuarioform.getRoles().stream().anyMatch(r -> "ROLE_USER".equals(r.getNombre()))){
-            
-            redirectAttributes.addFlashAttribute("noHayAdmin",true);
-            log.info("No se pudo editar el usuario");
+        //Verificamos que el usuario de la BD sea el último admin para evitar quitarle el rol
+        if (usuarioService.esUltimoAdmin(usuarioDB) &&
+                usuarioform.getRoles().stream().anyMatch(r -> "ROLE_USER".equals(r.getNombre()))) {
+
+            redirectAttributes.addFlashAttribute("noHayAdmin", true);
+            log.info("No se pudo editar el usuario: " + usuarioDB.getUsername() + " porque es el último admin");
             return "redirect:/admin/usuarios";
         }
 
+        //Obtenemos el nombre de usuario que está logueado al momento de hacer el guardado
+        String usernameLogueado = authentication.getName();
+        boolean esMismoUsuario = usernameLogueado.equals(usuarioform.getUsername());
+
+        //Reemplazamos los nuevos datos que ingresó el usuario directamente sobre el usuario de la BD
         usuarioDB.setUsername(usuarioform.getUsername());
         usuarioDB.setRoles(usuarioform.getRoles());
 
-        if(usuarioform.getPassword() != null && !usuarioform.getPassword().isBlank()){
+        //Verificamos si el usuario cambió la contraseña
+        if (usuarioform.getPassword() != null && !usuarioform.getPassword().isBlank()) {
             usuarioDB.setPassword(passwordEncoder.encode(usuarioform.getPassword()));
-            log.info("Se ha cambiado la contraseña del usuario con id:" +usuarioDB.getIdUsuario());
+            log.info("Se ha cambiado la contraseña del usuario con id:" + usuarioDB.getIdUsuario());
         }
 
-        log.info("Se ha actualizado el usuario con id: "+usuarioDB.getIdUsuario());
+        log.info("Se ha actualizado el usuario con id: " + usuarioDB.getIdUsuario());
+        //Guardamos el nuevo usuario en la BD
         usuarioService.saveUsuario(usuarioDB);
-        return "redirect:/admin/usuarios";
+
+        //Si el usuario se editó a sí mismo se lo manda a iniciar sesión de nuevo para aplicar los cambios
+        if (esMismoUsuario){
+            SecurityContextHolder.clearContext();
+            redirectAttributes.addFlashAttribute("usuarioEditado",true);
+            return "redirect:/login";
+        }
+
+            return "redirect:/admin/usuarios";
     }
 
 
